903fc6ccf096da82d4c2e73cdb6a6110bcd0896b
Artem Lysevych <a.lysevych@gmail.com>
2022-12-17 00:57:09 +0100

    Create empty Xcode project

---
+++ Pixie/Pixie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pixie
+//
+//  Created by Artem on 17/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
